afe5ae7ddc6648838ccfd2de7c4f98b66cc3bc8e
ComputeAce <timeastw@gmail.com>
2025-05-03 16:13:19 +0200

    added new login

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('login_user/', views.login_user, name='login_user')
 ]
--- base/views.py
+++ base/views.py
@@ -10,14 +10,18 @@ def home(request):
 
 
 def login(request):
+    return render(request, 'base/login.html')
+
+
+def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'Login successful!')
             return redirect('home')
         else:
-            messages.warning(request, 'Invalid username or password.')
-    return render(request, 'base/login.html')
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')
+    return render(request, 'login.html')
