597ace7a19e86eceb070d77bd7063d49ce6ebbd6
ComputeAce <timeastw@gmail.com>
2025-05-04 14:33:51 +0200

    added addes

--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('users/', views.users, name='users'),
-    path('login_user/', views.login_user, name='login_user')
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout-user/', views.logout_user, name='logout_user'),
+    path('create-user/', views.create_user, name='create_user'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 @login_required(login_url='login')
@@ -26,4 +27,37 @@ def login_user(request):
     return render(request, 'login.html')
 
 def users(request):
-    return render(request, 'base/users.html')
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'base/users.html', context)
+
+
+def create_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('user-email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = '1234'
+
+        check_email = User.objects.filter(email = email)
+        if check_email.exists():
+            messages.warning(request, 'Email exists')
+            return redirect('users')
+        
+        check_username = User.objects.filter(username = username)
+        if check_username.exists():
+            messages.warning(request, 'Username taken')
+            return redirect('users')
+
+        User.objects.create_user(username = username, email = email, first_name = first_name, last_name = last_name, password= password)
+        messages.success(request, 'User created')
+        return redirect('users')
+    
+
+def logout_user(request):
+    auth_logout(request)
+    messages.success(request, "Logged out successfully.")
+    return redirect('login')
