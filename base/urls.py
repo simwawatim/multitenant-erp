106151a7d81faf35ac6d8e3ea6a9236469091c9b
ComputeAce <timeastw@gmail.com>
2025-05-04 14:06:53 +0200

    add modal for adding users

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
+    path('users/', views.users, name='users'),
     path('login_user/', views.login_user, name='login_user')
 ]
